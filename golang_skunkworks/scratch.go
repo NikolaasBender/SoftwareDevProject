032b50b2e0ce01cfa00fb6cd518c9ac0b317a9db
Nick <superpineappleperson@gmail.com>
2019-03-01 22:16:58 -0700

    more work

--- golang_skunkworks/scratch.go
+++ golang_skunkworks/scratch.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"html/template"
+	//"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,12 +14,18 @@ import (
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	//THIS IS FROM THE OLD METHOD...THE BUILT IN METHOD
-	r.HandleFunc("/", general).Methods("GET")
+	//r.HandleFunc("/", general).Methods("GET")
 	//THIS IS THE NEW METHOD THAT WORKS
 	//TO FUTURE NICK: SEE IF YOU CAN PUT THIS INTO A FUNCTION
-	staticFileDirectory := http.Dir("./assets/")
-	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
-	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	// staticFileDirectory := http.Dir("./assets/")
+	// staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	r.HandleFunc("/", HomeHandler)
+	//YOU'RE GONNA WANNA CHANGE THESE. YOU MIGHT WANT PROJECTS TO BE UNDER USERS
+	//r.HandleFunc("/projects/{project}", ProjectHandler)
+	//r.HandleFunc("/user", UserHandler)
+
+	http.Handle("/", r)
 	return r
 }
 
@@ -31,9 +37,9 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func general(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, r.URL.Path[1:])
-}
+// func general(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, r.URL.Path[1:])
+// }
 
 //THE GERNERAL FORM OF A PAGE
 type Page struct {
@@ -49,30 +55,14 @@ func (p *Page) save() error {
 
 //THIS JUST DISPLAYS A TEXT FILE ON THE WEBPAGE
 //DEFINITELY USEFUL FOR STUFF - MAYBE IT CAN WORK WITH CSV?
-func rawHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w, "view", p)
-}
+// func rawHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/view/"):]
+// 	p, _ := loadPage(title)
+// 	renderTemplate(w, "view", p)
+// }
 
-//tHIS LOADS A .TXT FILE FOR VIEWING IN HTML
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, r, r.URL.Path[1:])
 }
