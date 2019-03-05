ce2250bc0f85ce620216d521be3097155c5d7004
Nick <superpineappleperson@gmail.com>
2019-03-05 14:52:24 -0700

    working on login

--- golang_skunkworks/scratch.go
+++ golang_skunkworks/scratch.go
@@ -15,6 +15,7 @@ import (
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/{page}", HomeHandler)
+	r.HandleFunc("/", IndexHandle)
 	//r.HandleFunc("/second", SecondHandler)
 	//r.HandleFunc("/third/{number}", ThirdHandler)
 	return r
@@ -24,15 +25,8 @@ func main() {
 
 	//WE NEED A ROUTER
 	r := newRouter()
-	fmt.Println("loop?")
 	//RUNS THE SERVER
 	log.Fatal(http.ListenAndServe(":5050", r))
-}
-
-//THE GERNERAL FORM OF A PAGE
-type Page struct {
-	Title string
-	Body  []byte
 }
 
 type PageVariables struct {
@@ -40,32 +34,46 @@ type PageVariables struct {
 	Time string
 }
 
+type ContactDetails struct {
+	Username string
+	Password string
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
 	w.WriteHeader(http.StatusOK)
 	pathVariables := mux.Vars(r)
 	fmt.Println("HOME HANDLER: '" + pathVariables["page"] + "'" + "'" + r.URL.Path + "'")
-	//http.ServeFile(w, r, "/index.html")
 	PageVars := PageVariables{ //store the date and time in a struct
 		Date: now.Format("02-01-2006"),
 		Time: now.Format("15:04:05"),
 	}
 
-	t, err := template.ParseFiles(pathVariables["page"])
+	t, _ := template.ParseFiles(pathVariables["page"])
 
-	if err != nil { // if there is an error
-		log.Print("template parsing error: ", err) // log it
-	}
-
-	err = t.Execute(w, PageVars)
-
-	if err != nil { // if there is an error
-  	  log.Print("template executing error: ", err) //log it
-  	}
-
+	t.Execute(w, PageVars)
 }
 
-func CatchAllHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandle(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func LoginHandle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	t, _ := template.ParseFiles("login.html")
+	if r.Method != http.MethodPost {
+		t.Execute(w, nil)
+		return
+	}
+
+	details := ContactDetails{
+		Username: r.FormValue("username"),
+		Password: r.FormValue("password"),
+	}
+
+	// do something with details
+	_ = details
+
+	t.Execute(w, struct{ Success bool }{true})
 }
