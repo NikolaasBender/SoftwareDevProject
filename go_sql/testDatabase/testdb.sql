ed02b7db3f60b0428ad1a888b16cebee93c6ccae
RS-Coop <simpson.r.cooper@gmail.com>
2019-03-09 16:05:55 -0700

    Serious test database and text file explaining how to restore and dump

--- go_sql/testDatabase/testdb.sql
+++ go_sql/testDatabase/testdb.sql
@@ -34,67 +34,15 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: user_login; Type: TABLE; Schema: public; Owner: postgres
+-- Name: projects; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.user_login (
-    username character varying(30),
-    password character varying(30)
+CREATE TABLE public.projects (
+    name text
 );
 
 
-ALTER TABLE public.user_login OWNER TO postgres;
-
---
--- Data for Name: user_login; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.user_login (username, password) FROM stdin;
-bob	123
-coolGuy5	pandas
-Alan	Paradise
-sentientRobot	killallhumans
-\.
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
--- Dumped by pg_dump version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
+ALTER TABLE public.projects OWNER TO postgres;
 
 --
 -- Name: user_login; Type: TABLE; Schema: public; Owner: postgres
@@ -154,10 +102,29 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.projects (name) FROM stdin;
+proj
+dev
+bello
+\.
+
+
+--
 -- Data for Name: user_login; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.user_login (username, password) FROM stdin;
+bob	123
+coolGuy5	pandas
+Alan	Paradise
+sentientRobot	killallhumans
+bob	123
+coolGuy5	pandas
+Alan	Paradise
+sentientRobot	killallhumans
 bob	123
 coolGuy5	pandas
 Alan	Paradise
