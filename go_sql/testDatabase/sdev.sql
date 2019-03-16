5e87e86d33f2936e07514bb96f1bd753fe0d74e7
RS-Coop <simpson.r.cooper@gmail.com>
2019-03-16 12:09:58 -0600

    Updated database moving forward

--- go_sql/testDatabase/sdev.sql
+++ go_sql/testDatabase/sdev.sql
@@ -34,17 +34,120 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: posts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    task integer NOT NULL,
+    title text,
+    users character varying(30) NOT NULL,
+    content text NOT NULL
+);
+
+
+ALTER TABLE public.posts OWNER TO postgres;
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.posts_id_seq OWNER TO postgres;
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.projects (
-    owner character varying(30),
+    id integer NOT NULL,
+    owner character varying(30) NOT NULL,
     name text NOT NULL,
-    members character varying(30)[]
+    completed_tasks integer[],
+    todo_tasks integer[],
+    inprogress_tasks integer[],
+    users character varying(30)[]
 );
 
 
 ALTER TABLE public.projects OWNER TO postgres;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.projects_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO postgres;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
+
+
+--
+-- Name: tasks; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tasks (
+    id integer NOT NULL,
+    project integer NOT NULL,
+    name text NOT NULL,
+    description text,
+    users character varying(30)[],
+    due_date date,
+    status smallint,
+    CONSTRAINT tasks_status_check CHECK (((status >= 0) AND (status <= 2)))
+);
+
+
+ALTER TABLE public.tasks OWNER TO postgres;
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.tasks_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tasks_id_seq OWNER TO postgres;
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.tasks_id_seq OWNED BY public.tasks.id;
+
 
 --
 -- Name: user_info; Type: TABLE; Schema: public; Owner: postgres
@@ -53,7 +156,9 @@ ALTER TABLE public.projects OWNER TO postgres;
 CREATE TABLE public.user_info (
     username character varying(30),
     name text NOT NULL,
-    bio text
+    bio text,
+    profile_image text,
+    banner_image text
 );
 
 
@@ -65,18 +170,54 @@ ALTER TABLE public.user_info OWNER TO postgres;
 
 CREATE TABLE public.user_login (
     username character varying(30) NOT NULL,
-    password character varying(30) NOT NULL,
-    email text NOT NULL
+    password integer NOT NULL
 );
 
 
 ALTER TABLE public.user_login OWNER TO postgres;
 
 --
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- Name: projects id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
+
+
+--
+-- Name: tasks id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tasks ALTER COLUMN id SET DEFAULT nextval('public.tasks_id_seq'::regclass);
+
+
+--
+-- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.posts (id, task, title, users, content) FROM stdin;
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.projects (owner, name, members) FROM stdin;
+COPY public.projects (id, owner, name, completed_tasks, todo_tasks, inprogress_tasks, users) FROM stdin;
+\.
+
+
+--
+-- Data for Name: tasks; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.tasks (id, project, name, description, users, due_date, status) FROM stdin;
 \.
 
 
@@ -84,7 +225,7 @@ COPY public.projects (owner, name, members) FROM stdin;
 -- Data for Name: user_info; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.user_info (username, name, bio) FROM stdin;
+COPY public.user_info (username, name, bio, profile_image, banner_image) FROM stdin;
 \.
 
 
@@ -92,16 +233,53 @@ COPY public.user_info (username, name, bio) FROM stdin;
 -- Data for Name: user_login; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.user_login (username, password, email) FROM stdin;
+COPY public.user_login (username, password) FROM stdin;
 \.
 
 
 --
--- Name: user_login user_login_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.user_login
-    ADD CONSTRAINT user_login_email_key UNIQUE (email);
+SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 1, false);
+
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.tasks_id_seq', 1, false);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tasks tasks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tasks
+    ADD CONSTRAINT tasks_pkey PRIMARY KEY (id);
 
 
 --
@@ -113,11 +291,27 @@ ALTER TABLE ONLY public.user_login
 
 
 --
+-- Name: posts posts_task_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_task_fkey FOREIGN KEY (task) REFERENCES public.tasks(id);
+
+
+--
 -- Name: projects projects_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.projects
     ADD CONSTRAINT projects_owner_fkey FOREIGN KEY (owner) REFERENCES public.user_login(username);
+
+
+--
+-- Name: tasks tasks_project_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tasks
+    ADD CONSTRAINT tasks_project_fkey FOREIGN KEY (project) REFERENCES public.projects(id);
 
 
 --
