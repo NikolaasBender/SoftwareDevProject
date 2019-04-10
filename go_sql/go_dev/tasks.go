60d67aaad4d9f017e09cbe4535318bc25d4157fe
Iva Majeticova <ivma2396@colorado.edu>
2019-04-09 19:21:59 -0600

    post/task go code

--- go_sql/go_dev/tasks.go
+++ go_sql/go_dev/tasks.go
@@ -1,29 +1,165 @@
 package go_dev
 
 import (
+  "database/sql"
+  _ "github.com/lib/pq"
   "fmt"
 )
 
-func createTask(project_name, name string) {
-  fmt.Println("Placeholder")
+func createTask(project_name,project_owner, task_name string,db *sql.DB)(bool) {
+  	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	
+	var parentID string
+  	var err error
+	
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+
+ 	sqlStatement2 := `INSERT INTO tasks(project,name,status)
+  	VALUES ($1, $2, 0)`
+
+  	_, err = db.Exec(sqlStatement2,parentID,task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
 
-func addTaskMembers(usernames ...string) {
-  fmt.Println("Placeholder")
+func addTaskMembers(project_name,project_owner, task_name,newMember,db *sql.DB)(bool) {
+	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	var parentID string
+  	var err error
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	
+	sqlStatement:= `UPDATE tasks
+  	SET users = users || '{$1}'
+  	WHERE project = $2 AND name = $3;`
+
+  	_, err = db.Exec(sqlStatement,newMember,parentID,task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
 
-func updateStatus(int) {
-  fmt.Println("Placeholder")
+func updateStatus(project_name,project_owner, task_name,status int,db *sql.DB)(bool) {
+  	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	var parentID string
+  	var err error
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	
+	sqlStatement:= `UPDATE tasks
+  	SET status = $1
+  	WHERE project = $2 AND name = $3;`
+
+  	_, err = db.Exec(sqlStatement,status,parentID,task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
 
-func addDescription() {
-  fmt.Println("Placeholder")
+func addDescription(project_name,project_owner, task_name, description,db *sql.DB) (bool){
+  	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	var parentID string
+  	var err error
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	
+	sqlStatement:= `UPDATE tasks
+  	SET description = $1
+  	WHERE project = $2 AND name = $3;`
+
+  	_, err = db.Exec(sqlStatement,description,parentID,task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
 
-func dueDate() {
-  fmt.Println("Placeholder")
+func dueDate(project_name,project_owner, task_name,dueDate,db *sql.DB)(bool) {
+  	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	var parentID string
+  	var err error
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	
+	sqlStatement:= `UPDATE tasks
+  	SET due_date = $1
+  	WHERE project = $2 AND name = $3;`
+
+  	_, err = db.Exec(sqlStatement,dueDate,parentID,task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
 
-func deleteTask() {
-  fmt.Println("Placeholder")
+func deleteTask(project_name,project_owner, task_name,db *sql.DB) {
+  	sqlStatement1:= `SELECT id FROM projects WHERE owner = $1 AND name = $2;`
+	
+	var parentID string
+  	var err error
+	err = db.QueryRow(sqlStatement1,project_owner,project_name).Scan(&parentID)
+
+ 	if(err == sql.ErrNoRows) {
+   		return false
+  	} else if (err != nil) {
+    	return false
+  	}
+	
+	sqlStatement := `DELETE FROM tasks
+  	WHERE  project= $1 AND name = $2;`
+
+  	var err error
+
+  	_, err = db.Exec(sqlStatement, parentID, task_name)
+
+  	if(err != nil) {
+    	return false
+  	}
+
+  	return true
 }
