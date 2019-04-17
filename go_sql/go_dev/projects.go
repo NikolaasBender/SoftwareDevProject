d0c32872abee3d8db2ca969fdce953c4cfd67b74
Nick <superpineappleperson@gmail.com>
2019-04-16 21:33:17 -0600

    dfs

--- go_sql/go_dev/projects.go
+++ go_sql/go_dev/projects.go
@@ -1,56 +1,56 @@
 package go_dev
 
-import(
-  "database/sql"
-  //"fmt"
-  _ "github.com/lib/pq"
+import (
+	"database/sql"
+	//"fmt"
+	_ "github.com/lib/pq"
 )
 
-func CreateProject(owner, name string, db *sql.DB) (bool) {
+func CreateProject(owner, name string, db *sql.DB) bool {
 
-  sqlStatement := `INSERT INTO projects(owner,name)
+	sqlStatement := `INSERT INTO projects(owner,name)
   VALUES ($1, $2)`
 
-  var err error
+	var err error
 
-  _, err = db.Exec(sqlStatement,owner,name)
+	_, err = db.Exec(sqlStatement, owner, name)
 
-  if(err != nil) {
-    return false
-  }
+	if err != nil {
+		return false
+	}
 
-  return true
+	return true
 }
 
-func AddProjectMembers(owner, name, newuser string, db *sql.DB) (bool) {
+func AddProjectMembers(owner, name, newuser string, db *sql.DB) bool {
 
-  sqlStatement := `UPDATE projects
+	sqlStatement := `UPDATE projects
   SET users = users || '{$1}'
   WHERE owner = $2 AND name = $3;`
 
-  var err error
+	var err error
 
-  _, err = db.Exec(sqlStatement,newuser,owner,name)
+	_, err = db.Exec(sqlStatement, newuser, owner, name)
 
-  if(err != nil) {
-    return false
-  }
+	if err != nil {
+		return false
+	}
 
-  return true
+	return true
 }
 
-func DeleteProject(owner, name string, db *sql.DB) (bool) {
+func DeleteProject(owner, name string, db *sql.DB) bool {
 
-  sqlStatement := `DELETE FROM projects
+	sqlStatement := `DELETE FROM projects
   WHERE owner = $1 AND name = $2;`
 
-  var err error
+	var err error
 
-  _, err = db.Exec(sqlStatement, owner, name)
+	_, err = db.Exec(sqlStatement, owner, name)
 
-  if(err != nil) {
-    return false
-  }
+	if err != nil {
+		return false
+	}
 
-  return true
+	return true
 }
